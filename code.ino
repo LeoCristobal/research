00aeb67088e6c8c28d5bddd8402d865f3a198815
LeoCristobal <leocristobal108@gmail.com>
2025-08-23 19:09:38 +0800

    fixing error

--- code.ino
+++ code.ino
@@ -1,81 +1,52 @@
-z/*
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# RFID MFRC522 / RC522 Library : https://github.com/miguelbalboa/rfid # 
-#                                                                     # 
-#                 Installation :                                      # 
-# NodeMCU ESP8266/ESP12E    RFID MFRC522 / RC522                      #
-#         D2       <---------->   SDA/SS                              #
-#         D5       <---------->   SCK                                 #
-#         D7       <---------->   MOSI                                #
-#         D6       <---------->   MISO                                #
-#         GND      <---------->   GND                                 #
-#         D1       <---------->   RST                                 #
-#         3V/3V3   <---------->   3.3V                                #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# If you are in a good mood, please subscriber Uteh Str YouTube Channel : https://www.youtube.com/channel/UCk8rZ8lhAH4H-75tQ7Ljc1A :) :) :) #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-*/
-
-//----------------------------------------Include the NodeMCU ESP8266 Library---------------------------------------------------------------------------------------------------------------//
-//----------------------------------------see here: https://www.youtube.com/watch?v=8jMr94B8iN0 to add NodeMCU ESP8266 library and board
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-
-//----------------------------------------Include the SPI and MFRC522 libraries-------------------------------------------------------------------------------------------------------------//
-//----------------------------------------Download the MFRC522 / RC522 library here: https://github.com/miguelbalboa/rfid
 #include <SPI.h>
 #include <MFRC522.h>
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
-#define SS_PIN D2  //--> SDA / SS is connected to pinout D2
-#define RST_PIN D1  //--> RST is connected to pinout D1
-MFRC522 mfrc522(SS_PIN, RST_PIN);  //--> Create MFRC522 instance.
+// RFID wiring
+#define SS_PIN D8   // SDA / SS → D8 (GPIO15)
+#define RST_PIN D0  // RST → D0 (GPIO16)
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-#define ON_Board_LED 2  //--> Defining an On Board LED, used for indicators when the process of connecting to a wifi router
+// On-board LED
+#define ON_Board_LED 2
 
-//----------------------------------------SSID and Password of your WiFi router-------------------------------------------------------------------------------------------------------------//
+// WiFi credentials
 const char* ssid = "T-Attack";
 const char* password = "likeaboss08";
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 int readsuccess;
 byte readcard[4];
 char str[32] = "";
 String StrUID;
 
-HTTPClient http; //--> Declare object of class HTTPClient
+HTTPClient http;
 WiFiClient client;
 
-//-----------------------------------------------------------------------------------------------SETUP--------------------------------------------------------------------------------------//
 void setup() {
-  Serial.begin(115200); //--> Initialize serial communications with the PC
-  SPI.begin();      //--> Init SPI bus
-  mfrc522.PCD_Init(); //--> Init MFRC522 card
+  Serial.begin(115200);
+  SPI.begin();          // SCK → D5, MOSI → D7, MISO → D6 (as per your wiring)
+  mfrc522.PCD_Init();
 
   delay(500);
 
-  WiFi.begin(ssid, password); //--> Connect to your WiFi router
+  WiFi.begin(ssid, password);
   Serial.println("");
     
-  pinMode(ON_Board_LED,OUTPUT); 
-  digitalWrite(ON_Board_LED, HIGH); //--> Turn off Led On Board
+  pinMode(ON_Board_LED, OUTPUT);
+  digitalWrite(ON_Board_LED, HIGH);
 
-  //----------------------------------------Wait for connection
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    //----------------------------------------Make the On Board Flashing LED on the process of connecting to the wifi router.
     digitalWrite(ON_Board_LED, LOW);
     delay(250);
     digitalWrite(ON_Board_LED, HIGH);
     delay(250);
   }
-  digitalWrite(ON_Board_LED, HIGH); //--> Turn off the On Board LED when it is connected to the wifi router.
-  //----------------------------------------If successfully connected to the wifi router, the IP Address that will be visited is displayed in the serial monitor
+  digitalWrite(ON_Board_LED, HIGH);
+
   Serial.println("");
   Serial.print("Successfully connected to : ");
   Serial.println(ssid);
@@ -85,70 +56,53 @@ void setup() {
   Serial.println("Please tag a card or keychain to see the UID !");
   Serial.println("");
 }
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
-//-----------------------------------------------------------------------------------------------LOOP---------------------------------------------------------------------------------------//
 void loop() {
-  // put your main code here, to run repeatedly
   readsuccess = getid();
  
-  if(readsuccess) {  
-  digitalWrite(ON_Board_LED, LOW);
-    String UIDresultSend, postData;
-    UIDresultSend = StrUID;
-   
-    //Post Data
-    postData = "UIDresult=" + UIDresultSend;
+  if (readsuccess) {  
+    digitalWrite(ON_Board_LED, LOW);
 
-    // Your_Host_or_IP = Your pc or server IP, example : 192.168.0.0 , if you are a windows os user, open cmd, then type ipconfig then look at IPv4 Address.
-    http.begin(client, "http://192.168.254.184/PROJECT/getUID.php");  //Specify request destination
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //Specify content-type header
+    String UIDresultSend = StrUID;
+    String postData = "UIDresult=" + UIDresultSend;
+
+    http.begin(client, "https://project-ny82.onrender.com/getUID.php");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
    
-    int httpCode = http.POST(postData);   //Send the request
-    String payload = http.getString();    //Get the response payload
+    int httpCode = http.POST(postData);
+    String payload = http.getString();
   
     Serial.println(UIDresultSend);
-    Serial.println(httpCode);   //Print HTTP return code
-    Serial.println(payload);    //Print request response payload
+    Serial.println(httpCode);
+    Serial.println(payload);
     
-    http.end();  //Close connection
+    http.end();
     delay(1000);
-  digitalWrite(ON_Board_LED, HIGH);
+    digitalWrite(ON_Board_LED, HIGH);
   }
 }
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
-//----------------------------------------Procedure for reading and obtaining a UID from a card or keychain---------------------------------------------------------------------------------//
 int getid() {  
-  if(!mfrc522.PICC_IsNewCardPresent()) {
-    return 0;
-  }
-  if(!mfrc522.PICC_ReadCardSerial()) {
-    return 0;
-  }
+  if (!mfrc522.PICC_IsNewCardPresent()) return 0;
+  if (!mfrc522.PICC_ReadCardSerial()) return 0;
  
-  
   Serial.print("THE UID OF THE SCANNED CARD IS : ");
   
-  for(int i=0;i<4;i++){
-    readcard[i]=mfrc522.uid.uidByte[i]; //storing the UID of the tag in readcard
+  for (int i = 0; i < 4; i++) {
+    readcard[i] = mfrc522.uid.uidByte[i];
     array_to_string(readcard, 4, str);
     StrUID = str;
   }   
   mfrc522.PICC_HaltA();
   return 1;
 }
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
-//----------------------------------------Procedure to change the result of reading an array UID into a string------------------------------------------------------------------------------//
 void array_to_string(byte array[], unsigned int len, char buffer[]) {
-    for (unsigned int i = 0; i < len; i++)
-    {
-        byte nib1 = (array[i] >> 4) & 0x0F;
-        byte nib2 = (array[i] >> 0) & 0x0F;
-        buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
-        buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
-    }
-    buffer[len*2] = '\0';
+  for (unsigned int i = 0; i < len; i++) {
+    byte nib1 = (array[i] >> 4) & 0x0F;
+    byte nib2 = (array[i] >> 0) & 0x0F;
+    buffer[i*2+0] = nib1 < 0xA ? '0' + nib1 : 'A' + nib1 - 0xA;
+    buffer[i*2+1] = nib2 < 0xA ? '0' + nib2 : 'A' + nib2 - 0xA;
+  }
+  buffer[len*2] = '\0';
 }
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
--- getUID.php
+++ getUID.php
@@ -1,5 +1,14 @@
 <?php
-	$UIDresult=$_POST["UIDresult"];
-	$Write="<?php $" . "UIDresult='" . $UIDresult . "'; " . "echo $" . "UIDresult;" . " ?>";
-	file_put_contents('/tmp/UIDContainer.php',$Write);
+if (isset($_POST['UIDresult']) && !empty($_POST['UIDresult'])) {
+    $UIDresult = $_POST['UIDresult'];
+    $Write = "<?php $" . "UIDresult='" . $UIDresult . "'; " . "echo $" . "UIDresult;" . " ?>";
+
+    // Save to tmp folder (ephemeral in Render)
+    file_put_contents('/tmp/UIDContainer.php', $Write);
+
+    // Response to ESP8266
+    echo "Received UID: " . $UIDresult;
+} else {
+    echo "No UID received";
+}
 ?>
